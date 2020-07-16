f1fcbf3fee0dbb74769f29cddcf60fb23cba3a90
Chen Gao <maxlin2905@gmail.com>
2020-07-16 22:31:57 +1000

    160720 night

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,23 @@
 const chatForm = document.getElementById('chat-form')
-
+const chatMessages = document.querySelector('.chat-messages')
+//Get username and room from URL
+const {username,room}=Qs.parse(location.search,{
+    ignoreQueryPrefix:true
+})
 const socket =io()
 
+//join chatroom
+
+socket.emit('joinRoom',{username,room})
+
+//message from server
 socket.on('message',message => {
     console.log(message)
+    outputMessage(message)
+
+    //scroll down
+ chatMessages.scrollTop = chatMessages.scrollHeight
+
 })
 
 //Message submit
@@ -13,5 +27,19 @@ chatForm.addEventListener('submit', e=> {
     const msg = e.target.elements.msg.value
     //Emit message to server
     socket.emit('chatMessage',msg)
-    
+    //clear input
+    e.target.elements.msg.value=''
+    e.target.elements.msg.focus()
 })
+
+//output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div')
+    
+    div.classList.add('message')
+    div.innerHTML=`<p class="meta">${message.username}<span> ${message.time}</span>
+    <p class="text">
+    ${message.text}
+    </p>`
+    chatMessages.appendChild(div)
+}
--- server.js
+++ server.js
@@ -2,29 +2,37 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio= require('socket.io')
+const formatMessage = require('./utils/message')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+const botName="Chat Bot"
 //set static folder
 app.use(express.static(path.join(__dirname,'public')))
 
+
 io.on('connection',socket =>{
     // Welcome current user
-    //message to the user only
-    socket.emit('message','Welcome to ChatCord')
+    socket.on('joinRoom',({username,room})=>{
+  //message to the user only
+  socket.emit('message',formatMessage(username,'Welcome to ChatCord'))
 
-    //message to all the users except the user
-    socket.broadcast.emit('message','A user has joined the chat')
+  //message to all the users except the user
+  socket.broadcast.emit('message',formatMessage(botName,'A user has joined the chat'))
 
+    })
+
+
+  
     //runs when client disconnects
     socket.on('disconnect', ()=> {
-        io.emit('message', 'A user has left the chat')
+        io.emit('message',formatMessage(botName, 'A user has left the chat'))
     })
 
     //listen for chatmessage
     socket.on('chatMessage',(msg)=>{
-        io.emit('message', msg)
+        io.emit('message',formatMessage('User', msg))
     })
 })
 
